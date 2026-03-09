eb785f4bf189e94ad8ab81eaa06e9e0aa74c79ea
maximilianjaramazovic <maxi.jaramazovic@gmail.com>
2026-03-09 17:49:05 -0400

    Added some checks for arguments, still needs implemntation to read the data table and initial message of the program

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 
+import sys
 def main():
-  print("First Program")
+  # Checking for input data table
+  args = sys.argv[1:]
+  if len(args) != 1:
+    print("Please run the program providing one argument, which specifys the path to the mta data table.")
+  else:
+    pass
+
+  # Starting program
+  print("""
+    Welcome to the subway program. n \
+    To begin, try typing 'help' to see the list of valid commands. 
+    """)
+
 
 
 
