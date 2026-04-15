46f8f79ad6870002b1034ad2add2c4f67dd2dab5
maximilianjaramazovic <maxi.jaramazovic@gmail.com>
2026-04-15 12:49:37 -0400

    Organized the file reading process. Created a Stations datatype to store the stations into which will be used to store items in a set. Code for these are not final yet.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,35 @@
 import sys
 import fileinput
 
+# Data types
+class Station:
+  name: str
+  # Division,
+  # Line,
+  # Station Name,
+  # Station Latitude,
+  # Station Longitude,
+  # Route1,Route2,Route3,Route4,Route5,Route6,Route7,Route8,Route9,Route10,Route11,
+  # Entrance Type,
+  # Entry,
+  # Exit Only,
+  # Vending,
+  # Staffing,
+  # Staff Hours,
+  # ADA,
+  # ADA Notes,
+  # Free Crossover,
+  # North South Street,
+  # East West Street,
+  # Corner,
+  # Entrance Latitude,
+  # Entrance Longitude,
+  # Station Location,
+  # Entrance Location
+  def __init__(self, line):
+    pass
+
+
 def main():
   # Checking for input data table
   arguments_list = sys.argv[1:]
@@ -8,13 +37,7 @@ def main():
     print("Please run the program providing one argument, which specifys the path to the mta data table.")
   else:
     file_path = arguments_list[1]
-    try:
-      with open(file_path) as f:
-        
-      pass
-      #note fot rebasing idk
-    except:
-      pass
+    station_data = initialize_data(file_path)
 
   # Starting program
   print("""
@@ -30,6 +53,26 @@ def main():
     else:
       print("Invalid option. Type 'help' to see the list of valid commands.")
     user_input = str(input("Enter option: "))
+
+def initialize_data(file_path):
+  try:
+    f = open(file_path)
+  except:
+    print ("Error: file not found. Please check the file path and try again.")
+  #ignore first line because that is the header
+  station_dict = {}
+  f.readline()
+  #each line get the station name, and insert into array of stations storing station datatypes
+  while(f.readable()):
+    line = f.readline()
+    #store each property
+    #if the next entry is the same station, add exit to exit array and entrence to enterance array
+    #if the next entry is different repeat initial process.
+    #if end of file, break loop
+    if line == "":
+      break
+  
+  return station_dict
 
 
 
